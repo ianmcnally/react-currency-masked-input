6c6d0a33a1c68ccf14e5ed9e6df9dfdc40dd20c8
Ian McNally <ianmcnally@users.noreply.github.com>
2017-06-09 21:22:43 -0400

    Use prop-types, closes #4

--- src/components/currency-input.js
+++ src/components/currency-input.js
@@ -1,4 +1,5 @@
-import React, { Component, PropTypes } from 'react'
+import React, { Component } from 'react'
+import PropTypes from 'prop-types'
 import { toCurrency } from '../services/currency-conversion'
 
 export default class CurrencyInput extends Component {
