fb79377dcf5bc315b5e4d5bbd8c7412e33ee8206
Ian McNally <imcnally@2u.com>
2019-04-29 11:47:31 -0400

    Upgrade dependencies

--- rollup.config.js
+++ rollup.config.js
@@ -1,8 +1,10 @@
 import babelPlugin from 'rollup-plugin-babel'
 
 export default {
-  entry: 'src/components/currency-input.js',
-  format: 'cjs',
-  dest: 'react-currency-masked-input.js',
+  input: 'src/components/currency-input.js',
+  output: {
+    format: 'cjs',
+    dest: 'react-currency-masked-input.js',
+  },
   plugins: [babelPlugin({ exclude: 'node_modules/**' })]
 }
